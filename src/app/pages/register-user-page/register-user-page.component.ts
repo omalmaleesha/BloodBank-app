296ece08fc81949b15a5baf6126b3b4e8b380e2f
omalmaleesha <omalmaleesha03@gmail.com>
2024-10-27 19:57:37 +0530

    initial commit

---
+++ src/app/pages/register-user-page/register-user-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-user-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './register-user-page.component.html',
+  styleUrl: './register-user-page.component.css'
+})
+export class RegisterUserPageComponent {
+
+}
